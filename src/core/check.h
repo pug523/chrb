d88b7b1a9257416b66695a95cd4a5e46670cc804
pugur <pugurmc@gmail.com>
2026-03-08 02:19:53 +0900

    feat: improve dcheck logging

--- src/core/check.h
+++ src/core/check.h
@@ -4,16 +4,32 @@
 
 #pragma once
 
-#include <cassert>
+#include <cstdlib>
+#include <print>
 
 namespace core {
 
-// TODO: replace assert with some better way
-
-#ifdef DEBUG
-#define dcheck(exp) assert(exp);
-#else
-#define dcheck(exp)
-#endif
+[[noreturn]] inline void dcheck_fail(const char* expr,
+                                     const char* file,
+                                     int line,
+                                     const char* func) {
+  std::print(stderr, "DCHECK failed: {}\n  at {}:{} ({})\n", expr, file, line,
+             func);
+  std::abort();
+}
 
 }  // namespace core
+
+#ifdef DEBUG
+#define dcheck(expr)                                            \
+  do {                                                          \
+    if (__builtin_expect(!(expr), 0)) {                         \
+      ::core::dcheck_fail(#expr, __FILE__, __LINE__, __func__); \
+    }                                                           \
+  } while (false)
+#else
+#define dcheck(expr) \
+  do {               \
+    (void)(expr);    \
+  } while (false)
+#endif

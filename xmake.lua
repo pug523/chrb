0419169bc0e310e25aa489b0976d3c6a3c3f32b0
pugur <pugurmc@gmail.com>
2026-03-10 17:42:28 +0900

    build: v0.2.0

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@
 -- which can be found in the LICENSE file.
 
 set_project("chrb")
-local project_version = "0.1.1"
+local project_version = "0.2.0"
 set_version(project_version)
 
 set_policy("build.ccache", true)

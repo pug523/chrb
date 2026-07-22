c238b11b1b5c6b6d112d028c541b93e61af2a25d
pugur <pugurmc@gmail.com>
2026-07-22 20:52:43 +0900

    fix: windows build failed with unused argument warning

--- src/core/mem/mapped_file.cc
+++ src/core/mem/mapped_file.cc
@@ -29,6 +29,7 @@ bool MappedFile::open(std::string_view path, size_t min_size, ColorMode color) {
   path_ = path;
 
 #if CHRB_BUILD_FLAG(IS_OS_WIN)
+  (void)(color);
   file_handle_ =
       CreateFileA(path_.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ,
                   nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
